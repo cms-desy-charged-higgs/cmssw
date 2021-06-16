d7b7c74905e54425ad6cc463691323a34789c694
David Brunner <david.brunner@desy.de>
2021-06-16 18:34:29 +0200

    Function doing MTS plots

---
+++ Alignment/OfflineValidation/interface/MTSplotter.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1D.h>
+#include <TH2F.h>
+#include <TCanvas.h>
+
+namespace MTS {
+    void produceCanvas(const std::string& fileNames);
+};

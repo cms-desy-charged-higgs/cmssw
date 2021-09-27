438352081e1867d4d39dffe681b96e34c41121b1
David Brunner <david.brunner@desy.de>
2021-09-27 18:54:17 +0200

    Proper config readout

--- Alignment/OfflineValidation/bin/MTSmerge.cc
+++ Alignment/OfflineValidation/bin/MTSmerge.cc
@@ -11,10 +11,10 @@
 
 #include <Alignment/OfflineValidation/interface/MTSplotter.h>
 
+namespace pt = boost::property_tree;
 
 int plotMTS(int argc, char *argv[]) {
-    // parse the command line
-    /*
+    //parse the command line
     AllInOneConfig::Options options;
     options.helper(argc, argv);
     options.parser(argc, argv);
@@ -22,15 +22,21 @@ int plotMTS(int argc, char *argv[]) {
     //Read in AllInOne json config
     pt::ptree main_tree;
     pt::read_json(options.config, main_tree);
-    */
 
     //Need variables read from config
-    std::string outDir = "/eos/home-d/dbrunner/www/Plots/NewMTS";
-    std::vector<std::string> files = {"TrackSplitting_Cosmics_final2017_newT.root", "TrackSplitting_Cosmics_UL17_fineAPEs_SD.root"};
-    std::vector<int> colors = {600, 632};
-    std::vector<int> styles = {2101, 2101};
-    std::vector<std::string> titles = {"Test1", "Test2"};
-    std::vector<std::string> alignments = {"Test1", "Test2"};
+    std::string outDir = main_tree.get<std::string>("output");
+    std::string legendTitle = main_tree.get<std::string>("validation.customrighttitle");
+
+    std::vector<std::string> files, titles, alignments;
+    std::vector<int> colors, styles;
+
+    for(const std::pair<std::string, pt::ptree>& alignment : main_tree.get_child("alignment")){
+        colors.push_back(alignment.second.get<int>("color"));
+        styles.push_back(alignment.second.get<int>("style"));
+        titles.push_back(alignment.second.get<std::string>("title"));
+        files.push_back(alignment.second.get<std::string>("input"));
+        alignments.push_back(alignment.first);
+    }
 
     std::vector<std::string> outFiles(files.size(), "");
 
@@ -38,10 +44,10 @@ int plotMTS(int argc, char *argv[]) {
     MTSPlotter plotter;
 
     for(std::size_t i = 0; i < files.size(); ++i){
-        outFiles[i] = plotter.ProduceHists(files.at(i), outDir, alignments.at(i));
+        outFiles[i] = plotter.ProduceHists(files.at(i), outDir, alignments.at(i), main_tree.get<float>("outliercut", 1.));
     }
 
-    plotter.Plot(outFiles, outDir, colors, styles, alignments);
+    plotter.Plot(outFiles, outDir, colors, styles, titles, legendTitle);
 
     return EXIT_SUCCESS;
 }
